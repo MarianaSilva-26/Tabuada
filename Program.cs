d6ca8b3a9971568705787eda73dc0280462b0d5e
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-11-22 20:10:07 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+         int numero; 
+
+         Console.WriteLine("Tabuada do numero: ");
+         numero = int.Parse(Console.ReadLine());
+         Console.WriteLine(); 
+         
+        for (int a = 1; a <= 10; a++)
+        {
+        Console.WriteLine(numero + " * " + a + " = " + numero * a); 
+        }   
+        }
+    }
+}
